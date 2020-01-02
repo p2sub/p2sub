d5b8d1a57a2e111cd429a663c7d9ae59daf8301d
Chiro Hiro <chiro@fkguru.com>
2020-01-02 11:11:39 +0700

    Add address layer

---
+++ address/address.go
@@ -0,0 +1,77 @@
+// Copyright 2019 Trần Anh Dũng <chiro@fkguru.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 		http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package address
+
+import (
+	"crypto/ed25519"
+
+	"github.com/btcsuite/btcutil/base58"
+)
+
+//Address node indentity
+type Address struct {
+	publicKey  ed25519.PublicKey
+	privateKey ed25519.PrivateKey
+}
+
+//GetAddress get public key of key pair
+func (a *Address) GetAddress() []byte {
+	if a.publicKey != nil && len(a.publicKey) == ed25519.PublicKeySize {
+		return []byte(a.publicKey)
+	} else if a.privateKey != nil && len(a.privateKey) == ed25519.PrivateKeySize {
+		if publicKey, ok := a.privateKey.Public().(ed25519.PublicKey); ok {
+			a.publicKey = publicKey
+		}
+		return []byte(a.publicKey)
+	}
+	return nil
+}
+
+//GetBase58Address get base58 encoded of public key
+func (a *Address) GetBase58Address() string {
+	if vk := a.GetAddress(); vk != nil {
+		return base58.Encode(a.publicKey)
+	}
+	return ""
+}
+
+//IsSignKey is this address contain private key and able to sign transaction
+func (a *Address) IsSignKey() bool {
+	return a.privateKey != nil
+}
+
+//Sign sign a message
+func (a *Address) Sign(message []byte) []byte {
+	if a.IsSignKey() {
+		signature := ed25519.Sign(a.privateKey, message)
+		signedMessage := make([]byte, len(message)+ed25519.SignatureSize)
+		copy(signedMessage[:ed25519.SignatureSize], signature[:])
+		copy(signedMessage[ed25519.SignatureSize:], message[:])
+		return signedMessage
+	}
+	return nil
+}
+
+//Verify signed a message
+func (a *Address) Verify(signedMessage []byte) bool {
+	if vk := a.GetAddress(); vk != nil {
+		signature := make([]byte, ed25519.SignatureSize)
+		message := make([]byte, len(signedMessage)-ed25519.SignatureSize)
+		copy(signature[:], signedMessage[:ed25519.SignatureSize])
+		copy(message[:], signedMessage[ed25519.SignatureSize:])
+		return ed25519.Verify(vk, message, signature)
+	}
+	return false
+}
---
+++ address/generator.go
@@ -0,0 +1,69 @@
+// Copyright 2019 Trần Anh Dũng <chiro@fkguru.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 		http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package address
+
+import (
+	"crypto/ed25519"
+	"encoding/hex"
+
+	"github.com/btcsuite/btcutil/base58"
+)
+
+//New Generate new address from random
+func New() *Address {
+	//If nil was set then crypto/rand will be used
+	if vk, pk, err := ed25519.GenerateKey(nil); err == nil {
+		return &Address{publicKey: vk,
+			privateKey: pk}
+	}
+	return nil
+}
+
+//FromBase58Address Import address from base58 address string for verification only
+func FromBase58Address(address string) *Address {
+	if vk := base58.Decode(address); len(vk) == ed25519.PublicKeySize {
+		return &Address{publicKey: vk}
+	}
+	return nil
+}
+
+//FromSeed generate address from seed
+func FromSeed(seed []byte) *Address {
+	pk := ed25519.NewKeyFromSeed(seed)
+	if vk, ok := pk.Public().(ed25519.PublicKey); ok {
+		return &Address{privateKey: pk,
+			publicKey: vk}
+	}
+	return nil
+}
+
+//FromPublicKey create address from public key
+func FromPublicKey(vk []byte) *Address {
+	if vk != nil && len(vk) == ed25519.PublicKeySize {
+		return &Address{publicKey: vk}
+	}
+	return nil
+}
+
+//FromHexSeed gernate from hex string of seed
+func FromHexSeed(seed string) *Address {
+	if len(seed)%2 == 1 {
+		seed = "0" + seed
+	}
+	if s, e := hex.DecodeString(seed); e == nil {
+		return FromSeed(s)
+	}
+	return nil
+}
