0d88119055de6fa63b2dfbc31cee9ed60f5d85a0
Chiro Hiro <chiro@fkguru.com>
2020-04-09 20:22:17 +0700

    Implement merkle binary tree

--- cmd/mb-tree/main.go
+++ cmd/mb-tree/main.go
@@ -1,75 +1,126 @@
 package main
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
 	"math/big"
+
+	"github.com/petar/GoLLRB/llrb"
 )
 
-// MerkleBinaryTree binary hash tree
-type MerkleBinaryTree struct {
-	hash     *big.Int
-	previous *MerkleBinaryTree
-	left     *MerkleBinaryTree
-	right    *MerkleBinaryTree
+//Digest size for hash key
+const (
+	HashKeySize = 32
+)
+
+//HashKey digiest of data
+type HashKey [HashKeySize]byte
+
+//Merkle node in merkle tree
+type MerkleNode struct {
+	letf   *MerkleNode
+	right  *MerkleNode
+	parent *MerkleNode
+	digest HashKey
 }
 
-func newRoot() *MerkleBinaryTree {
-	maxHash := hexToInt("ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff")
-	rootHash := new(big.Int)
-	rootHash.Rsh(maxHash, 1)
-	return &MerkleBinaryTree{hash: rootHash,
-		previous: nil,
-		left:     nil,
-		right:    nil}
+func bytesToHashKey(b []byte) (r HashKey) {
+	copy(r[:], b[:HashKeySize])
+	return r
 }
 
-func newLeaf(hexString string, previousLeaf *MerkleBinaryTree) *MerkleBinaryTree {
-	return &MerkleBinaryTree{hash: hexToInt(hexString),
-		previous: nil,
-		left:     nil,
-		right:    nil}
+//BinaryMerkleTree sorted and verifiable structure
+type BinaryMerkleTree struct {
+	tree       *llrb.LLRB
+	storage    map[HashKey][]byte
+	merkleRoot *MerkleNode
 }
 
-func hexToInt(hexString string) *big.Int {
-	tmp := new(big.Int)
-	tmp.SetString(hexString, 16)
-	return tmp
-}
-
-func isGt(x *big.Int, y *big.Int) bool {
-	return x.Cmp(y) == 1
-}
-
-func isLt(x *big.Int, y *big.Int) bool {
-	return x.Cmp(y) == -1
-}
-
-func isEq(x *big.Int, y *big.Int) bool {
-	return x.Cmp(y) == 0
-}
-
-func (m *MerkleBinaryTree) insert(n *MerkleBinaryTree) {
-	inserted := false
-	w := m
-	for inserted == false {
-		if isGt(n.hash, m.hash) {
-			if w.right == nil {
-				w.right = n
-				break
-			}
-			w = m.right
-		} else if isLt(n.hash, m.hash) {
-			w = m.left
-		} else {
-			inserted = true
-			break
-		}
-		if w == nil {
-			w = c
-			inserted = true
-		}
+//New create new new binary merkle tree
+func New() *BinaryMerkleTree {
+	return &BinaryMerkleTree{
+		tree:    llrb.New(),
+		storage: make(map[HashKey][]byte),
 	}
 }
 
-func main() {
+func (t *BinaryMerkleTree) CalculateMerkleRoot() {
+	t.tree.AscendGreaterOrEqual(t.tree.Min(), func(i llrb.Item) bool {
+		l, _ := i.(Leaf)
+		fmt.Println(l.Value.Text(16))
+		return true
+	})
+}
 
+//Insert data to binary merkle tree
+func (t *BinaryMerkleTree) Insert(data []byte) {
+	//Calculate digest
+	h := sha256.New()
+	h.Write(data)
+	d := h.Sum(nil)
+	k := bytesToHashKey(d)
+	fmt.Println(hex.EncodeToString(d))
+	//Insert digest to llrb tree
+	t.tree.ReplaceOrInsert(LeafFromDigest(k))
+
+	//Insert digest to storage
+	t.storage[k] = data
+}
+
+//Leaf leaf
+type Leaf struct {
+	Value big.Int
+}
+
+//Less is curren item less than?
+func (l Leaf) Less(than llrb.Item) (r bool) {
+	if b, ok := than.(Leaf); ok && l.Value.CmpAbs(&b.Value) == -1 {
+		r = true
+	}
+	return
+}
+
+//LeafFromDigest create leafe from digest
+func LeafFromDigest(k HashKey) Leaf {
+	n := new(big.Int)
+	d := make([]byte, 32)
+	copy(d, k[:])
+	n.SetBytes(d)
+	return Leaf{Value: *n}
+}
+
+/*
+func newLeaf(val big.Int) Leaf {
+	return Leaf{Value: val}
+}
+
+func newBigInt(val int) big.Int {
+	n := new(big.Int)
+	n.SetInt64(int64(val))
+	return *n
+}*/
+
+func main() {
+	/*
+		tree := llrb.New()
+		tree.ReplaceOrInsert(newLeaf(newBigInt(1)))
+		tree.ReplaceOrInsert(newLeaf(newBigInt(2)))
+		tree.ReplaceOrInsert(newLeaf(newBigInt(3)))
+		tree.ReplaceOrInsert(newLeaf(newBigInt(4)))
+		//tree.DeleteMin()
+		//tree.Delete(llrb.Inf(1))
+		fmt.Println("Item value: ", tree.Min(), tree.Max())
+		tree.AscendGreaterOrEqual(tree.Min(), func(i llrb.Item) bool {
+			k, _ := i.(Leaf)
+			fmt.Println("Item value: ", k.Value.Text(10))
+			return true
+		})
+	*/
+	tree := New()
+	tree.Insert([]byte("I'm"))
+	tree.Insert([]byte("invincible!"))
+	tree.Insert([]byte("I am"))
+	tree.Insert([]byte("Ironman!"))
+	tree.CalculateMerkleRoot()
 }
